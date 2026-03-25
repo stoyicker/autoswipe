3b907969afdaf638e3bfe402dc750b3509744dc2
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2026-03-25 22:09:34 +0100

    Add support for detecting running out of profiles on Badoo and Bumdle

--- content/badoo.js
+++ content/badoo.js
@@ -1,4 +1,8 @@
 const engine = new AutoSwipeEngine({
   platformId: 'badoo',
   key: 'ArrowRight',
+
+  beforeSwipe() {
+    if (document.body.innerText.includes("That's all for now!")) return false;
+  },
 });
--- content/bumble.js
+++ content/bumble.js
@@ -1,4 +1,8 @@
 const engine = new AutoSwipeEngine({
   platformId: 'bumble',
   key: 'ArrowRight',
+
+  beforeSwipe() {
+    if (document.body.innerText.includes("You're all caught up!")) return false;
+  },
 });
